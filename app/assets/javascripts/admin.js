704b5750e029f9e0b8de825ccf807daef421916c
Rocco Galluzzo <mail@roccogalluzzo.com>
2013-10-16 15:31:31 +0200

    fixed bug reload product list js

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,25 +1,3 @@
-$(document).ready(function(){
-	$.fn.peity.defaults.line = {
-		colour: "#c6d9fd",
-		strokeColour: "#4d89f9",
-		strokeWidth: 1,
-		delimiter: ",",
-		height: 38,
-		max: null,
-		min: 0,
-		width: 200
-	}
-	$(".line").peity("line")
-	$( ".product-stats-cell" ).hover(
-  function() {
-    $( this ).addClass( "hover" );
-  }, function() {
-    $( this ).removeClass( "hover" );
-  }
-);
-
-});
-
    function showAlert(type, message){
          $('.alert-box')
          .html('<div class="alert alert-'+ type + '"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>');
