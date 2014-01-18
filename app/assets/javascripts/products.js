a5d18b18b0df780d22a5b1f01e0ed0c1dc883dba
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-01-18 18:08:06 +0100

    Changed paypal window mode to lightbox
    In previous version after a payment the :returlUrl redirect was not
    working

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   .bind("ajax:success", function(evt, data, status, xhr){
 
     var url = "https://www.sandbox.paypal.com/webapps/adaptivepayment/flow/pay?expType=light&payKey=" + data.pay_key
-    var paypal_flow= new PAYPAL.apps.DGFlow({expType: 'instant'});
+    var paypal_flow= new PAYPAL.apps.DGFlow({expType: 'light'});
     paypal_flow.startFlow(url);
   })
 
