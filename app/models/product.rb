7a136bb51fbf266c32c67c23fb7c7a68b9419866
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-02-15 22:18:46 +0100

    Add s3 upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,9 @@ class Product < ActiveRecord::Base
 
   has_attached_file :file,
   :url => 'download/p/:token',
-  :path => ':rails_root/uploads/products/:customer_id/:id.:style.:extension'
+  :path => ':rails_root/uploads/products/:customer_id/:id.:style.:extension',
+  :storage => :s3,
+  :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
   has_attached_file :thumb, :styles => { :small => "72x72>" }
 
@@ -17,4 +19,9 @@ class Product < ActiveRecord::Base
   validates :price_cents, :numericality => {
     :greater_than => 0.50
   }
+  def s3_credentials
+    {:bucket => "up-sell-ms",
+      :access_key_id => "AKIAI564OZSI6YDCGVFA",
+      :secret_access_key => "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"}
+  end
 end
