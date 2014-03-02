a41b5986772468848b8509c45a0d1b717a7f3b02
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-03-02 20:07:05 +0100

    changed puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,13 @@
-deamonize true
-pidfile '/tmp/pids/puma.pid'
-state_path '/tmp/up-sell.stats'
+daemonize true
+pidfile '/tmp/puma.pid'
 bind 'unix:///tmp/up-sell.sock'
 
 threads 0, 6
-workers 1
-activate_control_app 'unix:///var/run/pumactl.sock', { no_token: true }
 
+environment 'production'
 on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
-  end
+require "active_record"
+cwd = File.dirname(__FILE__)+"/.."
+ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RACK_ENV"]])
 end
