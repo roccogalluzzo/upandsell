6cff3b3f718723622657617614713860a7e4ae53
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-03-03 00:55:22 +0100

    add profile

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
   task :export do
    on roles(:app) do
-    run "cd #{current_path} && #{sudo} foreman export upstart /etc/init -a #{application} -u #{user} -l /var/#{application}/log"
+    run "cd #{current_path} && #{sudo} foreman export upstart /etc/init -a #{application} -u #{user} -l /var/www/shared/#{application}/log"
   end
 end
 desc "Start the application services"
@@ -45,9 +45,9 @@ end
 namespace :deploy do
   task :restart do
    on roles(:app) do
-    #foreman.export
+    foreman.export
     # on OS X the equivalent pid-finding command is `ps | grep '/puma' | head -n 1 | awk {'print $1'}`
-    run "(kill -s SIGUSR1 $(ps -C ruby -F | grep '/puma' | awk {'print $2'})) || #{sudo} service #{application} restart"
+   run "(kill -s SIGUSR1 $(ps -C ruby -F | grep '/puma' | awk {'print $2'})) || #{sudo} service #{application} restart"
 
     # foreman.restart # uncomment this (and comment line above) if we need to read changes to the procfile
   end
