1ac9db632db7ee318be4fe9d1c994431eeb95757
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-03-02 15:24:30 +0100

    Update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,2 +1,14 @@
-threads 1, 6
+deamonize true
+pidfile '/tmp/pids/puma.pid'
+state_path '/tmp/up-sell.stats'
+bind 'unix:///tmp/up-sell.sock'
+
+threads 0, 6
 workers 1
+activate_control_app 'unix:///var/run/pumactl.sock', { no_token: true }
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
