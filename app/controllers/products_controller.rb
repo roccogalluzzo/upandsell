a810e6f06fbbe01e4ba1f00621233ac4a3e38ab9
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-03-04 14:00:39 +0100

    changed pay mill payment

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,34 +10,37 @@ class ProductsController < ApplicationController
   end
 
   def pay
-     @user  = User.find(@product.user_id)
-    Paymill.api_key = @user.credit_card_token
-    @product = Product.find(params[:product_id])
-    pay = Paymill::Transaction.create amount: @product.price.cents,
-    currency: @product.price_currency.upcase, token: params[:token]
-    token = ''
-    if pay.status == 'closed' && params[:email].present?
-      order = @product.orders.build(
-        email: params[:email],
-        payment_type: 'paymill',
-        payment_token: pay.id,
-        status: 'completed',
-        amount_cents: @product.price.cents,
-        amount_currency: @product.price.currency,
-        )
-      order.save
-      url = download_product_url(order.token)
-      update_user_products(order.product.id, order.token)
-      render json: { status: 'completed', url: url }
-      return
-    else
-      render json: { status: 'failed'}
-    end
-  end
+   @product = Product.find(params[:product_id])
+   @user  = User.find(@product.user_id)
+   Paymill.api_key = @user.credit_card_token
 
-  def paypal
-    @product = Product.find(params[:product_id])
-    @user  = User.find(@product.user_id)
+   payment = Paymill::Payment.create(token: params[:token])
+
+   pay = Paymill::Transaction.create(amount: @product.price.cents,
+   currency: @product.price_currency.upcase, payment: payment)
+
+   if pay.status == 'closed' && params[:email].present?
+    order = @product.orders.build(
+      email: params[:email],
+      payment_type: 'paymill',
+      payment_token: pay.id,
+      status: 'completed',
+      amount_cents: @product.price.cents,
+      amount_currency: @product.price.currency,
+      )
+    order.save
+    url = download_product_url(order.token)
+    update_user_products(order.product.id, order.token)
+    render json: { status: 'completed', url: url }
+    return
+  else
+    render json: { status: 'failed'}
+  end
+end
+
+def paypal
+  @product = Product.find(params[:product_id])
+  @user  = User.find(@product.user_id)
    #Paypal logic
    paypal = PayPal::SDK::AdaptivePayments.new
    req = paypal.BuildPay(
@@ -97,13 +100,13 @@ def show
   if !@downloads and is_user_product?(@product.id)
     @order = Order.find_by token: session[:user_products][@product.id]
     if @order.present?
-    @downloads =  @order.n_downloads
-  end
+      @downloads =  @order.n_downloads
+    end
   end
   @paypal = @product.user.paypal_status
   @credit_card =  @product.user.credit_card_status
   if request.browser?
-Metric::Products.new(@product.id).incr_visits
+    Metric::Products.new(@product.id).incr_visits
   end
 end
 
