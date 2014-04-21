fda90710ef46c6cc91dc86059581d43f65ebbd4f
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-04-21 17:26:59 +0200

    dump assets

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,11 +1,25 @@
 class SiteController < ApplicationController
-layout "site"
+  layout "site"
 
-def privacy
+  def privacy
 
   end
 
   def terms
 
   end
+
+  def unsubscribe
+   if user = User.read_access_token(params[:signature])
+    user.update_attribute :email_opt_in, false
+    render text: "You have been unsubscribed"
+  else
+    render text: "Invalid Link"
+  end
+end
+
+def test
+ @user = User.find 1
+
+end
 end
