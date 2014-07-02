a38bbf7df83c5a22ae6b44faf955fe81d71c40bc
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-07-03 00:08:25 +0200

    add froala editor
    IS better than others

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@
 //= require raphael/raphael
 //= require morris.js/morris
 //= require zeroclipboard/ZeroClipboard.min.js
-//= require pagedown_bootstrap
+//= require froala_editor.min.js
 //= require_tree ./user
 
 $(document).ready(Up.init);
