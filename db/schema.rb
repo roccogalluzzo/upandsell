a67e2ff3504b09833b19bf2b04cbbdb8fc337d1a
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-07-11 22:06:35 +0200

    add limit sales

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140706220745) do
+ActiveRecord::Schema.define(version: 20140711195803) do
 
   create_table "coupons", force: true do |t|
     t.integer  "product_id",             null: false
@@ -83,11 +83,12 @@ ActiveRecord::Schema.define(version: 20140706220745) do
     t.string   "preview"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "sales_limit"
   end
 
-  add_index "products", ["file_key"], name: "index_products_on_file_key", unique: true
-  add_index "products", ["slug"], name: "index_products_on_slug", unique: true
-  add_index "products", ["user_id"], name: "index_products_on_user_id"
+  add_index "products", ["file_key"], name: "index_products_on_file_key", unique: true, using: :btree
+  add_index "products", ["slug"], name: "index_products_on_slug", unique: true, using: :btree
+  add_index "products", ["user_id"], name: "index_products_on_user_id", using: :btree
 
   create_table "referrals", force: true do |t|
     t.integer  "referer_id"
@@ -143,8 +144,8 @@ ActiveRecord::Schema.define(version: 20140706220745) do
     t.text     "custom_email_message"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
