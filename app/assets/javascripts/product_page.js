4383ccfd8178f89cfe9d1881d588eb610e9fb51c
byterussian <rocco@galluzzo.me>
2014-08-02 23:12:14 +0200

    add input color

--- app/assets/javascripts/product_page.js
+++ app/assets/javascripts/product_page.js
@@ -18,7 +18,7 @@
     $('input.cc-num').payment('formatCardNumber');
     $('input.cc-exp').payment('formatCardExpiry');
     $('input.cc-cvc').payment('formatCardCVC');
-    //  ProductPage.Form.setValidation();
+    ProductPage.Form.setValidation();
   },
   submit: function() {
     ProductPage.Animations.show_form_processing();
@@ -28,7 +28,127 @@
       window.setTimeout(  ProductPage.Animations.show_form_success, 3000 );
     }
     return false;
-  }
+  },
+    open: function() {
+    ProductPage.Modal.open();
+    $('input.cc-num').payment('formatCardNumber');
+    $('input.cc-exp').payment('formatCardExpiry');
+    $('input.cc-cvc').payment('formatCardCVC');
+    ProductPage.Form.setValidation();
+    $(document).on('click','#js-btn-paypal', ProductPage.Paypal.request);
+    return false;
+  },
+  subbmit: function(event){
+
+    return false
+    cc_num = $('input.cc-num').val().replace(/\s+/g, '');
+    cc_exp = $('input.cc-exp').payment('cardExpiryVal')
+    cc_cvc = $('input.cc-cvc').val();
+    product = ProductPage.Form.productInfo();
+    ProductPage.Form.token(cc_num, cc_exp, cc_cvc, product.price, product.currency,
+      ProductPage.Form.pay)
+    return false;
+  },
+  productInfo: function() {
+   product = {};
+   $.ajax({
+    url: '/checkout/pay_info',
+    type: 'GET',
+    dataType: 'json',
+    data: {product_id: opts.productId},
+    async: false,
+    success: function(d) { product = d }
+  });
+   return product;
+ },
+ token: function(cc_num, cc_exp, cc_cvc, amount, currency, response) {
+  paymill.createToken({
+    number:         cc_num,
+    exp_month:      cc_exp.month,
+    exp_year:       cc_exp.year,
+    cvc:            cc_cvc,
+    amount_int:     amount,
+    currency:       currency
+  },
+  response);
+},
+pay: function(error, result) {
+  $.ajax({
+    url: '/checkout/pay',
+    type: 'POST',
+    dataType: 'json',
+    data: {product_id: $('.buy-form').data('product-id'),
+    token: result.token,
+    email: $('input.cc-email').val()},
+    success: ProductPage.Form.success,
+    error: ProductPage.Form.error
+  });
+},
+success: function(data) {
+ $('.js-btn-pay').each(function(){ this.disabled = false; });
+ $('.btn-buy').hide();
+ $('.cc-error').hide();
+ $('.btn-download').removeClass('hidden');
+ $('.btn-download').prop("href", data.url);
+ ProductPage.Modal.set('download');
+},
+error: function(d) {
+ $('.js-btn-pay').each(function(){ this.disabled = false;});
+ $('.processing').hide();
+ $('.message').hide();
+ $('.cc-error').show();
+},
+validationErrors: function(errorMap, errorList) {
+
+  $.each(this.validElements(), function (index, element) {
+    var $element = $(element);
+    $element.data("title", "")
+    .tooltip("destroy");
+    $element.removeClass("has-error");
+  });
+   // Create new tooltips for invalid elements
+   $.each(errorList, function (index, error) {
+    var $element = $(error.element);
+    $element.tooltip("destroy")
+    .data("title", error.message)
+    .data("placement", "bottom")
+    .tooltip();
+    $element.addClass("has-error");
+  });
+ },
+ setValidation: function() {
+  jQuery.validator.addMethod("ccNumberValid", function(value, element) {
+    return $.payment.validateCardNumber(value);
+  }, " Credit Card Number is Invalid.");
+  jQuery.validator.addMethod("ccExpValid", function(value, element) {
+    data = $.payment.cardExpiryVal(value);
+    return $.payment.validateCardExpiry(data.month, data.year);
+  }, " Credit Card Data is Invalid.");
+
+  $('#js-checkout-form').validate({
+    submitHandler: ProductPage.Form.submit,
+    rules : {
+      "cc-num" : { ccNumberValid : true,
+        required: true
+      },
+      "cc-email": {
+        required: true,
+        email: true
+      },
+      "cc-exp": {
+        required: true,
+        ccExpValid: true
+      },
+      "cc-cvc": {
+        required: true,
+        digits: true,
+        minlength: 3,
+        maxlength: 4
+      }
+    },
+    showErrors: ProductPage.Form.validationErrors
+  });
+}
 };
 
 ProductPage.Events = {
@@ -195,129 +315,6 @@ close: function() {
   $.fn.custombox('close');
 }
 
-}
-
-ProductPage.Foorm = {
-  open: function() {
-    ProductPage.Modal.open();
-    $('input.cc-num').payment('formatCardNumber');
-    $('input.cc-exp').payment('formatCardExpiry');
-    $('input.cc-cvc').payment('formatCardCVC');
-    ProductPage.Form.setValidation();
-    $(document).on('click','#js-btn-paypal', ProductPage.Paypal.request);
-    return false;
-  },
-  submit: function(event){
-
-    return false
-    cc_num = $('input.cc-num').val().replace(/\s+/g, '');
-    cc_exp = $('input.cc-exp').payment('cardExpiryVal')
-    cc_cvc = $('input.cc-cvc').val();
-    product = ProductPage.Form.productInfo();
-    ProductPage.Form.token(cc_num, cc_exp, cc_cvc, product.price, product.currency,
-      ProductPage.Form.pay)
-    return false;
-  },
-  productInfo: function() {
-   product = {};
-   $.ajax({
-    url: '/checkout/pay_info',
-    type: 'GET',
-    dataType: 'json',
-    data: {product_id: opts.productId},
-    async: false,
-    success: function(d) { product = d }
-  });
-   return product;
- },
- token: function(cc_num, cc_exp, cc_cvc, amount, currency, response) {
-  paymill.createToken({
-    number:         cc_num,
-    exp_month:      cc_exp.month,
-    exp_year:       cc_exp.year,
-    cvc:            cc_cvc,
-    amount_int:     amount,
-    currency:       currency
-  },
-  response);
-},
-pay: function(error, result) {
-  $.ajax({
-    url: '/checkout/pay',
-    type: 'POST',
-    dataType: 'json',
-    data: {product_id: $('.buy-form').data('product-id'),
-    token: result.token,
-    email: $('input.cc-email').val()},
-    success: ProductPage.Form.success,
-    error: ProductPage.Form.error
-  });
-},
-success: function(data) {
- $('.js-btn-pay').each(function(){ this.disabled = false; });
- $('.btn-buy').hide();
- $('.cc-error').hide();
- $('.btn-download').removeClass('hidden');
- $('.btn-download').prop("href", data.url);
- ProductPage.Modal.set('download');
-},
-error: function(d) {
- $('.js-btn-pay').each(function(){ this.disabled = false;});
- $('.processing').hide();
- $('.message').hide();
- $('.cc-error').show();
-},
-validationErrors: function(errorMap, errorList) {
-
-  $.each(this.validElements(), function (index, element) {
-    var $element = $(element);
-    $element.data("title", "")
-    .tooltip("destroy");
-    $element.parent().removeClass("has-error");
-  });
-   // Create new tooltips for invalid elements
-   $.each(errorList, function (index, error) {
-    var $element = $(error.element);
-    $element.tooltip("destroy")
-    .data("title", error.message)
-    .data("placement", "bottom")
-    .tooltip();
-    $element.parent().addClass("has-error");
-  });
- },
- setValidation: function() {
-  jQuery.validator.addMethod("ccNumberValid", function(value, element) {
-    return $.payment.validateCardNumber(value);
-  }, " Credit Card Number is Invalid.");
-  jQuery.validator.addMethod("ccExpValid", function(value, element) {
-    data = $.payment.cardExpiryVal(value);
-    return $.payment.validateCardExpiry(data.month, data.year);
-  }, " Credit Card Data is Invalid.");
-
-  $('#cc-form').validate({
-    submitHandler: ProductPage.Form.submit,
-    rules : {
-      "cc-num" : { ccNumberValid : true,
-        required: true
-      },
-      "cc-email": {
-        required: true,
-        email: true
-      },
-      "cc-exp": {
-        required: true,
-        ccExpValid: true
-      },
-      "cc-cvc": {
-        required: true,
-        digits: true,
-        minlength: 3,
-        maxlength: 4
-      }
-    },
-    showErrors: ProductPage.Form.validationErrors
-  });
-}
 }
 
 ProductPage.Paypal = {
