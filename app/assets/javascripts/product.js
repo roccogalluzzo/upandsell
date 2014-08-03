bf059414c16b69c059c88cdadd6445e07a8fd069
byterussian <rocco@galluzzo.me>
2014-08-03 12:14:02 +0200

    bump assets

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,10 +1,6 @@
 // Product landing page related js
-//= require jquery-simulate-ext/libs/bililiteRange.js
 //= require jquery
 //= require jquery_ujs
-//= require jquery-simulate-ext/libs/jquery.simulate.js
-//= require jquery-simulate-ext/src/jquery.simulate.ext.js
-//= require jquery-simulate-ext/src/jquery.simulate.key-sequence.js
 //= require excanvas.compiled.js
 //= require canvg.js
 //= require polyclip.js
