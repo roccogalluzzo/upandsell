d117bd1e62715fbde72fcce8ebe9192ede6c22fc
byterussian <rocco@galluzzo.me>
2014-08-03 12:01:32 +0200

    show product size on landing

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,6 +12,7 @@ class Product < ActiveRecord::Base
 
   before_create do
     self.slug = (Time.now.to_i + rand(1..1000)).to_s(36)
+    self.file_info = {size: ''}
   end
 
   before_save do
@@ -35,13 +36,16 @@ class Product < ActiveRecord::Base
 
   def url
     S3File.url(self.file_key)
- end
+  end
 
- def file_name
-  File.basename(self.file_key) if self.file_key
-end
+  def file_name
+    File.basename(self.file_key) if self.file_key
+  end
 
-def self.request(name)
-  S3File.request(name)
-end
+  def file_size
+    self.file_info[:size] || 0
+  end
+  def self.request(name)
+    S3File.request(name)
+  end
 end
