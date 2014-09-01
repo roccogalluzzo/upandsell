d0ebc003b05f973d90c75ee1b0ebae4bc57d3388
byterussian <rocco@galluzzo.me>
2014-09-01 10:04:38 +0200

    aggiunta signup page

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -1,7 +1,25 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap
+//= require jquery.validation/jquery.validate.js
 //= require frontend/animations
 $(document).ready(function() {
+ $(".simple_form").validate({ showErrors: function(errorMap, errorList) {
+  $.each(this.validElements(), function (index, element) {
+    var $element = $(element);
+    $element.data("title", "")
+    .tooltip("destroy");
+    $element.removeClass("has-error");
+  });
+   // Create new tooltips for invalid elements
+   $.each(errorList, function (index, error) {
+    var $element = $(error.element);
+    $element.tooltip("destroy")
+    .data("title", error.message)
+    .data("placement", "bottom")
+    .tooltip();
+    $element.addClass("has-error");
+  });
+ }});
  Animations.init();
 });
