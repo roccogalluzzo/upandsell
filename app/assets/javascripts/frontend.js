c537a1cd0a2e5b1850bf82ce7427234414225ddb
byterussian <rocco@galluzzo.me>
2014-09-02 17:37:10 +0200

    hbb

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -5,6 +5,15 @@
 //= require ScrollMagic/js/jquery.scrollmagic
 //= require ScrollMagic/js/jquery.scrollmagic.debug.js
 //= require frontend/animations
+
+var controller = new ScrollMagic();
+$('#checkout h3').hide();
+$('#checkout p').hide();
+$('.mock_iphone').hide();
+$('.mock-checkout').hide();
+$('.payments-section .section-illustration').hide();
+$('.payments-section .section-text').hide();
+
 $(document).ready(function() {
  $(".simple_form").validate({ showErrors: function(errorMap, errorList) {
   $.each(this.validElements(), function (index, element) {
--- app/assets/javascripts/frontend/animations.js
+++ app/assets/javascripts/frontend/animations.js
@@ -2,8 +2,12 @@
 
   Animations.init = function() {
  // init controller
+
  var controller = new ScrollMagic();
+
+
  Animations.Scenes.onLoading();
+
 // assign handler "scene" and add it to Controller
 var checkout_text = new ScrollScene({
   reverse: false,
@@ -33,27 +37,16 @@ payments_section.addIndicators();
 
 Animations.Scenes = {
   onLoading: function(){
+
+
     var animate_btns = function() {
-      $('.btn-more').show().addClass('animated fadeInUp');
-      $('.btn-get-started').show().addClass('animated fadeInUp');
+      $('.btn-more').removeClass('hidden-md hidden-lg').addClass('animated fadeInUp');
+      $('.btn-get-started').removeClass('hidden-md hidden-lg').addClass('animated fadeInUp');
     };
     var animate_shadows = function(n) {
      $('.shadow-'+ n).show().addClass('animated fadeIn');
    };
-
-   $('.btn-more').hide();
-   $('.shadow-1').hide();
-   $('.shadow-2').hide();
-   $('#checkout h3').hide();
-   $('#checkout p').hide();
-   $('.btn-get-started').hide();
-   $('.mock_iphone').hide();
-   $('.mock-checkout').hide();
-   $('.payments-section .section-illustration').hide();
-    $('.payments-section .section-text').hide();
-   $('h1').addClass('animated fadeInUp');
-   $('h2').addClass('animated fadeInUp');
-   setTimeout(animate_btns, 500);
+   animate_btns();
    setTimeout(function(){animate_shadows(2)}, 1100);
    setTimeout(function(){animate_shadows(1)}, 1600);
 
@@ -68,7 +61,7 @@ onCheckoutImage: function(){
 },
 onPayments: function(){
   $('.payments-section  .section-illustration').show().addClass('animated fadeInUp');
-   $('.payments-section .section-text').show().addClass('animated fadeInUp');
+  $('.payments-section .section-text').show().addClass('animated fadeInUp');
 },
 
 }
