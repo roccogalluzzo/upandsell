27aa08dd36b6d2ff24c9987cf00df5a7ec3aee09
byterussian <rocco@galluzzo.me>
2014-09-04 08:28:23 +0200

    uu

--- app/assets/javascripts/frontend/animations.js
+++ app/assets/javascripts/frontend/animations.js
@@ -1,134 +1,154 @@
 (function ($, Animations, undefined) {
 
   Animations.init = function() {
- // init controller
 
- var controller = new ScrollMagic();
+    $('.btn-more').on('click', function(){
+      $('html,body').animate({scrollTop: $('#checkout').offset().top}, 900);
+      return false;
+    });
+    Animations.reset();
+    var controller = new ScrollMagic();
+    Animations.Scenes.loading();
 
+    var checkout_text = new ScrollScene({
+      reverse: false,
+      triggerElement: '#checkout',
+      offset: -300})
+    .addTo(controller)
+    .on("start", Animations.Scenes.checkout);
 
- Animations.Scenes.onLoading();
+    var checkout_image = new ScrollScene({
+      reverse: false,
+      triggerElement: '#checkout',
+      offset: -30})
+    .addTo(controller)
+    .on("start", Animations.Scenes.checkoutImage);
 
-// assign handler "scene" and add it to Controller
-var checkout_text = new ScrollScene({
-  reverse: false,
-  triggerElement: '#checkout',
-  offset: -300}).addTo(controller)
-.on("start", Animations.Scenes.onCheckout);
-checkout_text.addIndicators();
+    var payments_section = new ScrollScene({
+      reverse: false,
+      triggerElement: '#checkout',
+      offset: 240})
+    .addTo(controller)
+    .on("start", Animations.Scenes.payments);
 
-var checkout_image = new ScrollScene({
-  reverse: false,
-  triggerElement: '#checkout',
-  offset: -30}).addTo(controller)
-.on("start", Animations.Scenes.onCheckoutImage);
+    var features_section = new ScrollScene({
+      reverse: false,
+      triggerElement: '.payments-section',
+      offset: 230})
+    .addTo(controller)
+    .on("start", Animations.Scenes.features);
 
-checkout_image.addIndicators();
+    var stats_section = new ScrollScene({
+      reverse: false,
+      triggerElement: '.features-section',
+      offset: 570})
+    .addTo(controller)
+    .on("start", Animations.Scenes.stats);
 
+    var pricing_section = new ScrollScene({
+      reverse: false,
+      triggerElement: '.teaser-section',
+      offset: 355})
+    .addTo(controller)
+    .on("start", Animations.Scenes.pricing);
 
-var payments_section = new ScrollScene({
-  reverse: false,
-  triggerElement: '#checkout',
-  offset: 240}).addTo(controller).on("start", Animations.Scenes.onPayments);
-payments_section.addIndicators();
+    pricing_section.addIndicators();
+    checkout_text.addIndicators();
+    stats_section.addIndicators();
+    features_section.addIndicators();
+    payments_section.addIndicators();
+    checkout_image.addIndicators();
 
-var features_section = new ScrollScene({
-  reverse: false,
-  triggerElement: '.payments-section',
-  offset: 230}).addTo(controller).on("start", Animations.Scenes.onFeatures);
-features_section.addIndicators();
+  };
 
-var stats_section = new ScrollScene({
-  reverse: false,
-  triggerElement: '.features-section',
-  offset: 570}).addTo(controller).on("start", Animations.Scenes.onStats);
-stats_section.addIndicators();
+  Animations.reset = function(){
+   var hide_el = [
+   '#checkout h3',
+   '#checkout p',
+   '.mock_iphone',
+   '.mock-checkout',
+   '.payments-section .section-illustration',
+   '.payments-section .section-text',
+   '.feature-box img',
+   '.feature-box h5',
+   '.feature-box p',
+   '.ad-logo',
+   '.plan-green',
+   '.plan-blue'
+   ];
+   jQuery.each(hide_el, function(i, el){
+    $(el).hide();
+  });
+ };
 
+ Animations.Scenes = {
+  loading: function(){
 
-var pricing_section = new ScrollScene({
-  reverse: false,
-  triggerElement: '.teaser-section',
-  offset: 355}).addTo(controller).on("start", Animations.Scenes.onPricing);
-
-pricing_section.addIndicators();
-};
-
-Animations.Scenes = {
-  onLoading: function(){
-    var animate_btns = function() {
-      $('.btn-more').removeClass('hidden-animation').addClass('animated fadeInUp');
-      $('.btn-get-started').removeClass('hidden-animation').addClass('animated fadeInUp');
-    };
-    var animate_shadows = function(n) {
-     $('.shadow-'+ n).show().addClass('animated fadeIn');
-   };
-   animate_btns();
+   $('.btn-more').removeClass('hidden-animation').addClass('animated fadeInUp');
+   $('.btn-get-started').removeClass('hidden-animation').addClass('animated fadeInUp');
    setTimeout(function(){
      $('.browser-preview').removeClass('hidden').addClass('animated fadeInUp');
-   }, 250);
-   setTimeout(function(){animate_shadows(2)}, 1100);
-   setTimeout(function(){animate_shadows(1)}, 1600);
-$('.small-cloud-left-t').addClass('animate-cloud-left');
+   },250);
+   setTimeout(function(){$('.shadow-2').show().addClass('animated fadeIn');}, 1100);
+   setTimeout(function(){$('.shadow-1').show().addClass('animated fadeIn');}, 1600);
+
  },
- onCheckout: function(){
+ checkout: function(){
+
   $('#checkout p').show().addClass('animated fadeInUp');
   $('#checkout h3').show().addClass('animated fadeInUp');
   $('.mock_iphone').show().addClass('animated fadeInUp');
+
 },
-onCheckoutImage: function(){
+checkoutImage: function(){
+
  $('.mock-checkout').show().addClass('animated fadeInUp');
+
 },
-onPayments: function(){
+payments: function(){
+
   $('.payments-section  .section-illustration').show().addClass('animated fadeInUp');
   $('.payments-section .section-text').show().addClass('animated fadeInUp');
+
 },
 
-onFeatures: function(){
+features: function(){
+
   function show_box_icon(el) {
-    $(el).find('img').removeClass('hidden-animation').addClass('animated fadeInLeft');
+    $(el).find('img').show().addClass('animated fadeInLeft');
   };
   function show_box_text(el) {
-
-    $(el).find('h5').removeClass('hidden-animation').addClass('animated fadeInLeft');
-    $(el).find('p').removeClass('hidden-animation').addClass('animated fadeInLeft');
-
+    $(el).find('h5').show().addClass('animated fadeInLeft');
+    $(el).find('p').show().addClass('animated fadeInLeft');
   };
 
 
   $('.feature-box').each(function(i, el){
-    setTimeout(function(){show_box_icon(el);}, 500*(i));
+    setTimeout(function(){show_box_icon(el);}, 400*(i));
   });
   setTimeout(function(){
     $('.feature-box').each(function(i, el){
-      setTimeout(function(){show_box_text(el);}, 500*(i));
+      setTimeout(function(){show_box_text(el);}, 400*(i));
     });
-  }, 1800);
+  }, 1600);
 
 },
-onStats: function(){
+stats: function(){
+
   function show_stat_icon(el) {
-    $(el).removeClass('hidden-animation').addClass('animated fadeInUp');
+    $(el).show().addClass('animated fadeInUp');
   };
-
-
-
   $('.ad-logo').each(function(i, el){
     setTimeout(function(){show_stat_icon(el);}, 500*(i));
   });
 
-
 },
-onPricing: function(){
-  function show_stat_icon(el) {
+pricing: function(){
 
-  };
-
-
-
-
-    setTimeout(function(){
-      $('.plan-green').removeClass('hidden-animation').addClass('animated fadeInUp');}, 500);
-     setTimeout(function(){
-      $('.plan-blue').removeClass('hidden-animation').addClass('animated fadeInUp');}, 1000);
+  setTimeout(function(){
+    $('.plan-green').show().addClass('animated fadeInUp');}, 500);
+  setTimeout(function(){
+    $('.plan-blue').show().addClass('animated fadeInUp');}, 1000);
 
 
 
