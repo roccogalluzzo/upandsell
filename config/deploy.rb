442e290e88eaa6ac9f884dca5ea6d343b1ae36b7
Rocco Galluzzo <mail@roccogalluzzo.com>
2014-12-23 16:48:18 +0100

    h

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 set :deploy_to, '/var/www/upandsell'
 set :deploy_via, :remote_cache
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle}
