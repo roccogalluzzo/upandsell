67abadfc2a549c71d10991b74937a2be281b6bc3
Rocco Galluzzo <mail@roccogalluzzo.com>
2015-02-21 16:03:15 +0100

    add log entries

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Upandsell::Application.initialize!
+
+if Rails.env.staging?
+  Rails.logger = Le.new('8c6f300e-38a2-49ce-8b44-6b9a3abdf98b', debug: true)
+else
+  Rails.logger = Le.new('8c6f300e-38a2-49ce-8b44-6b9a3abdf98b')
+end
